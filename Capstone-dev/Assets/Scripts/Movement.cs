1c9eb0121b6fb7cf3541bcfa9bddb22e7bb40026
junliangli <jl4661@g.rit.edu>
2018-10-10 15:35:22 -0400

    base movement
    base movement

---
+++ Capstone-dev/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float WalkSpeed = 1f;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // float value = Input.GetAxis("Left Y");
+        //Debug.Log(value);
+        transform.Translate(Input.GetAxis("Left X") * 0.05f *WalkSpeed, Input.GetAxis("Left Y") * 0.05f * WalkSpeed, 0);
+       
+
+
+    }
+}
