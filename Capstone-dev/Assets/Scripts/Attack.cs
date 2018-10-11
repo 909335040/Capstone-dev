c0c1d029891a03781762b277ae8584b58e5f7325
junliangli <junliangli@users.noreply.github.com>
2018-10-10 22:10:28 -0400

    change dir and  add target

--- Capstone-dev/Assets/Scripts/Attack.cs
+++ Capstone-dev/Assets/Scripts/Attack.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject RightAimIcon = null;
+
+    // Use this for initialization
+    void Start () {
+        RightAimIcon = Instantiate(RightAimIcon, transform);
+        RightAimIcon.transform.position = transform.position;
+        RightAimIcon.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetAxis("Right X") != 0  || Input.GetAxis("Right Y") != 0 ){
+            float tempx = Input.GetAxis("Right X");
+            float tempy = Input.GetAxis("Right Y");
+
+            Vector3 tempvector = new Vector3(tempx, tempy, 0);
+            tempvector = tempvector.normalized;
+            RightAimIcon.transform.localPosition = tempvector;
+        }
+
+        Debug.Log(RightAimIcon.transform.position);
+    }
 }
